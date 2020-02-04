cc0f459b7e6f3cb45892e956ff877086afd27fb7
vicente98fg <43605917+vicente98fg@users.noreply.github.com>
2020-02-04 09:06:55 +0100

    3 Controllers nuevos (App, Actividades, Equipos)

---
+++ proyectoPiscina/src/Controller/EquiposController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EquiposController extends AbstractController
+{
+    /**
+     * @Route("/equipos", name="equipos")
+     */
+    public function index()
+    {
+
+        return $this->render('equipos/index.html.twig', [
+            'controller_name' => 'EquiposController',
+        ]);
+    }
+
+    
+}
